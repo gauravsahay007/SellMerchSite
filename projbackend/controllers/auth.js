7d813993c6a43d87537c4de9f367ec3e3132b983
gauravsahay007 <gauravsahay2468@gmail.com>
2023-03-20 20:41:41 +0530

    Updated old findOne() method to new and added signin route

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -72,7 +72,7 @@ exports.signup = (req,res) =>{
 
         //  projection - specifies the list of fields to be displayed in the returned document. 
 
-        User.findOne({email}, (err,user) => {
+        User.findOne({email}).then((user,err)=> {
             if(err){
                 res.status(400).json({
                     error: "User email doesnot exists"
